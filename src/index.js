48d9fc8fd9d5f26b60a50d98e36bbcc033d82414
Pidopryhora Vitaliy <vpidopryhora@gmail.com>
2021-07-07 17:44:57 +0300

    107 - 15-00

--- src/index.js
+++ src/index.js
@@ -1,26 +1,21 @@
-import { createStore } from "redux";
+import { createStore, bindActionCreators } from "redux";
 import reducer from "./reducer";
-import {inc, dec, res} from "./actions";
+import * as actions from "./actions";
 
 const store = createStore(reducer);
 const {dispatch} = store;
 
-const bindActionCreator = (creator, dispatch) => {
-  return () => {
-    return () => dispatch(creator())
-  }
-};
+console.log(new Date());
 
-const incDispatch = () => dispatch(inc());
-const decDispatch = () => bindActionCreator(dec, dispatch);
+//const bindActionCreator = (creator, dispatch) => (...args) => dispatch(creator(...args));
+
+const {inc, dec, res} = bindActionCreators(actions, dispatch);
 
 const update = () => {
   document.getElementById('counter').textContent = store.getState();
 }
 store.subscribe(update);
 
-document.getElementById('inc').addEventListener('click', incDispatch);
-document.getElementById('dec').addEventListener('click', decDispatch);
-document.getElementById('res').addEventListener('click', () => {
-  dispatch(res());
-})
+document.getElementById('inc').addEventListener('click', inc);
+document.getElementById('dec').addEventListener('click', dec);
+document.getElementById('res').addEventListener('click', res);
